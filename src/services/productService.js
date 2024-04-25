12790ca0fb5b9b622df75bb6384744c66f27c53b
Sasidhar3159 <chavali.sasidhar@students.iiit.ac.in>
2024-04-25 17:06:08 +0530

    new changes
    Co-authored-by: Gowlapalli Rohit <ROHIT32767@users.noreply.github.com>

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,51 +1,20 @@
 
-// const ServiceRegistryClient = require('../utils/serviceRegistry');
-// const { CustomError } = require('../utils/error');
+const ServiceRegistryClient = require('../utils/serviceRegistry');
+const { CustomError } = require('../utils/error');
+const axios = require('axios');
 
-// async function fetchProductDetails(productId) {
-//   try {
-//     const ProductServiceUrl = await ServiceRegistryClient.getInstance().getUrl('Product')
-//     const response = await fetch(`${ProductServiceUrl}/api/get-product`, {
-//       method: "GET",
-//     });
+async function increaseProductQuantity(items){
+  try {
+    const ProductServiceUrl = await ServiceRegistryClient.getInstance().getUrl('Product');
+    const newObject = {
+      cart : items
+    }
+    const response = await axios.put(`${ProductServiceUrl}/api/product/refund`, newObject);
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching product details:", error.message);
+    throw error;
+  }
+}
 
-//     if (!response.ok) {
-//       throw new CustomError('HTTP error! in fetching product details', 500, false);
-//     }
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching product details:", error.message);
-//     throw error;
-//   }
-// }
-
-// // adding this function for testing purpose
-
-// async function getProductDetails(productId) {
-//   const products = {
-//       'product-id-1': {
-//           productId: 'product-id-1',
-//           name: 'Product One',
-//           price: 10.00,
-//           quantity: 1
-//       },
-//       'product-id-2': {
-//           productId: 'product-id-2',
-//           name: 'Product Two',
-//           price: 20.00,
-//           quantity: 1
-//       }
-//   };
-
-//   // Check if the product exists in the dummy data
-//   if (products[productId]) {
-//       return products[productId];
-//   } else {
-//       // Throw an error if no product matches the provided ID
-//       throw new CustomError(`Product not found for ID: ${productId}`, 404, false);
-//   }
-// }
-
-// module.exports = { fetchProductDetails, getProductDetails };
+module.exports = {increaseProductQuantity};
